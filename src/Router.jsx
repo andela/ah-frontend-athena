5d6457e249f8b1a8b54cb77276f7d95b4bad4bdf
Ntale Shadik <39732510+Nta1e@users.noreply.github.com>
2019-01-10 10:31:36 +0300

    chore(setup Redux): create folder/file structure for redux (#1)
    - Add folders for actions, reducers, and the store
    - Create an index.js file in reducers folder to combine all reducers
    - Create an index.js file in the store folder to create the store
    - create a actionTypes.js file in the actions folder to declare action
    types
    - Wrap all the returned data in App.js in a provider for the components
    to access the store
    
    [Delivers #162163220]

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NotFound from './views/notFound/NotFound'
 import Signup from './views/auth/signup/Signup'
@@ -6,11 +8,13 @@ import Login from './views/auth/Login/Login'
 
 const Routes = () =>(
     <Router>
+        <Provider store = {store}>
         <Switch>
             <Route path='/' component={Login} exact />
             <Route path='/signup' component={Signup} exact />
             <Route path='*' component={NotFound}/>
-        </Switch>        
+        </Switch>
+        </Provider>        
     </Router>
 )
 
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from '../reducers'
+
+const middleWare = [thunk]
+
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(
+        applyMiddleware(...middleWare)
+        )
+    );
+export default store;
